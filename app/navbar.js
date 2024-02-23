a1c53d65375a849c9ff845edd09f881a68a61c93
최원영 <eorkfclsrn@naver.com>
2024-02-23 09:30:22 +0900

    Update navbar.js

--- app/navbar.js
+++ app/navbar.js
@@ -7,19 +7,19 @@ export default function Navbar() {
       <div className="w-full fixed bottom-0 z-10 bg-white shadow border-t-2">
         <div id="tabs" className="flex justify-between">
           <Link href="/" className="flex flex-col items-center justify-center w-full pt-2 pb-1">
-              <Image src="/home.png" alt="Home" width={50} height={50} priority />
+              <Image src="/home.png" alt="Home" width={50} height={50} priority={true} />
           </Link>
           <Link href="/calender" className="flex flex-col items-center justify-center w-full pt-2 pb-1">
-              <Image src="/calender.png" width={50} height={50} priority />
+              <Image src="/calender.png" width={50} height={50} priority={true} />
           </Link>
           <Link href="/search" className="flex flex-col items-center justify-center w-full pt-2 pb-1">
-              <Image src="/search.png" width={50} height={50} priority />
+              <Image src="/search.png" width={50} height={50} priority={true} />
           </Link>
           <Link href="/favorite" className="flex flex-col items-center justify-center w-full pt-2 pb-1">
-              <Image src="/favorite.png" width={50} height={50} priority />
+              <Image src="/favorite.png" width={50} height={50} priority={true} />
           </Link>
           <Link href="/mypage" className="flex flex-col items-center justify-center w-full pt-2 pb-1">
-              <Image src="/user.png" width={50} height={50} priority />
+              <Image src="/user.png" width={50} height={50} priority={true} />
           </Link>
         </div>
       </div>
