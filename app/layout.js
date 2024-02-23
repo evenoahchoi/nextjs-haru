580118b6ca7c275c064f64aabed73e2f466019f8
evenoahchoi <eorkfclsrn@naver.com>
2024-02-23 09:36:55 +0900

    update

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./footer";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "하루명언",
   description: "하루명언",
